b7b74521259d5fdd2d7f93e8ca6d050dc763686e
ananthu <anandu@Ananthan-MacBook-Pro.local>
2023-04-06 23:20:04 +0530

    Adding UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,27 @@
+import React from 'react';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';  
+import {data} from './data';
 
 function App() {
   return (
     <div className="App">
-      Project Setup
+      <Navbar/>
+      <div className="main">
+        <div className='tabs'>
+        <div className='tab'>Movies</div>
+        <div className='tab'>Favorites</div>
+        </div>
+      </div>
+
+      <div className='list'>
+        {
+          data.map((movie)=>(
+            <MovieCard movie={movie}/>
+          ))
+        }
+      </div>
+
     </div>
   );
 }
