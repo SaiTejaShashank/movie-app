ad73d489d44f2cf23ad7ed71e2fe26f1d92880ed
ananthu <anandu@Ananthan-MacBook-Pro.local>
2023-04-07 00:06:54 +0530

    Actions Reducers Store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
 
 
+const store=createStore(movies);
+console.log('store',store);
+console.log('state',store.getState());
+
+store.dispatch({
+  type:'ADD_MOVIES',
+  movies:[{name:'superman'}]
+})
+console.log(store.getState())
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
