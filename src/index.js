dbe7dd2a1e62781b40307384a01aa2f30da44df0
ananthu <anandu@Ananthan-MacBook-Pro.local>
2023-04-07 13:29:06 +0530

    Middlewares

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
+import { createStore,applyMiddleware} from 'redux';
 
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers'; //export default used so any name can be given
 
-const store=createStore(rootReducer);
+
+const logger=function(obj){
+  return function(next){
+    return function(action){
+      console.log('ACTION_TYPE=',action.type)
+      next(action); //calls next middlewares if available or dispatches action if not there
+    }
+  }
+}
+const store=createStore(rootReducer,applyMiddleware(logger));
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
